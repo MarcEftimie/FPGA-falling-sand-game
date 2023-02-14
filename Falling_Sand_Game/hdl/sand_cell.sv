2327c7437fb66c469b4c866ea400615aabf572fc
MarcEftimie <marc.eftimie@gmail.com>
2023-02-13 23:03:51 -0800

    vga

---
+++ Falling_Sand_Game/hdl/sand_cell.sv
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+`default_nettype none
+
+module sand_cell 
+    # (
+        parameter ADDR_WIDTH = 8,
+        parameter DATA_WIDTH = 8
+    ) (
+        input logic clk_i,
+        input logic write_en,
+        input logic [DATA_WIDTH-1:0] cell_status,
+        input logic [ADDR_WIDTH-1:0] current_address_i,
+        output logic [ADDR_WIDTH-1:0] write_address_o,
+        output logic [DATA_WIDTH-1:0] write_data_o
+    );
+
+    always_comb begin
+        if (cell_status) begin
+            if ()
+        end
+    end
+
+endmodule
---
+++ Falling_Sand_Game/vram_sync_generator.py
@@ -0,0 +1,8 @@
+f = open("vram_sync.txt", "w")
+f.truncate(0)
+for i in range(0, 307200):
+    if (i == 320):
+        f.write(f"1\n")
+    else:
+        f.write(f"0\n")
+f.close()
